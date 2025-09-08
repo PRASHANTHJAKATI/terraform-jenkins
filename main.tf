3f1173726de5d755d420ea7d7c18e08b8997d117
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-08 20:53:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,59 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = "172.16.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "172.16.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
   tags = {
     Name = "main"
   }
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "jenkins-sg-2022" {
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "172.16.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "main-public-subnet"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "main-public-rt"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "jenkins_sg" {
   name        = var.security_group
   description = "security group for Ec2 instance"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 8080
@@ -22,41 +63,45 @@ resource "aws_security_group" "jenkins-sg-2022" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- # outbound from jenkis server
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = var.security_group
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
+  ami                         = var.ami_id
+  key_name                    = var.key_name
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
     Name = var.tag_name
   }
 }
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
+# Elastic IP
+resource "aws_eip" "myFirstInstance_eip" {
   instance = aws_instance.myFirstInstance.id
-tags= {
+  vpc      = true
+
+  tags = {
     Name = "my_elastic_ip"
   }
 }
