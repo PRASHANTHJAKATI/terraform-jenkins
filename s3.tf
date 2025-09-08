ce0d34a97a294dc666da7e8d0352e8607e95744d
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-08 21:10:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,16 +1,9 @@
-provider "aws" {
-  region     = "us-east-1"  # or your preferred region
-  access_key = "AKIATE2YZM62ORTAZREW"       # optional if using environment variables
-  secret_key = "6U49mZCKHMUXRfM/Uz5pRRHuZV+YqfCr7mHf2aIY"       # optional if using environment variables
-
-
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"   # for state locking
+    encrypt        = true
   }
-  
-  tags = var.tags
 }
